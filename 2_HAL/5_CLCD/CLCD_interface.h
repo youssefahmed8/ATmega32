5e87a8c154636d07ae37fa89f66b2de069b5d7ec
Youssef Ahmed <ahmedyoussef645@gmail.com>
2023-12-05 02:28:28 +0200

    Updating

--- 2_HAL/5_CLCD/CLCD_interface.h
+++ 2_HAL/5_CLCD/CLCD_interface.h
@@ -12,7 +12,16 @@
 #define CLCD_INTERFACE_H_
 #include "STD_TYPES.h"
 
-
+#define clcd_Clear           	 0x01          // replace all characters with ASCII 'space'
+#define clcd_Home            	 0x02          // return cursor to first position on first line
+#define clcd_EntryMode      	 0x06          // shift cursor from left to right on read/write
+#define clcd_DisplayOff     	 0x08          // turn display off
+#define clcd_DisplayOn      	 0x0C 		   // display on, cursor off, don't blink character
+#define clcd_Display_CursorOn 	 0x0E		   // display on, cursor on, don't blink character
+#define clcd_FunctionReset  	 0x30          // reset the LCD
+#define clcd_FunctionSet8bit 	 0x38          // 8-bit data, 2-line display, 5 x 7 font
+#define clcd_SetCursor      	 0x80          // set cursor position (First address of DDRAM)
+#define clcd_CGRAM				 0x40		   // set AC at first address of CGRam
 
 void CLCD_voidSendCommand(u8 Copy_u8Command);
 
@@ -20,7 +29,13 @@ void CLCD_voidSendCommand(u8 Copy_u8Command);
 void CLCD_voidSendData(u8 Copy_u8Data);
 
 void CLCD_Init(void);
-void CLCD_SendString(const u8 *ptString);
+
+void CLCD_SendPosition(u8 Copy_u8Row,u8 Copy_u8Col);
+
+void CLCD_SendString(const u8 *Copy_u8ptString);
+
+void CLCD_WriteSpecialCharacter(u8 Copy_u8Row,u8 Copy_u8Col);
+
 
 
 
--- 2_HAL/5_CLCD/CLCD_program.c
+++ 2_HAL/5_CLCD/CLCD_program.c
@@ -11,11 +11,14 @@
 
 #include "STD_TYPES.h"
 #include "DIO_interface.h"
-
+#include "CLCD_Speacialchar.h"
 #include "CLCD_config.h"
 #include "CLCD_private.h"
 #include "CLCD_interface.h"
 #include "util/delay.h"
+
+
+
 
 void CLCD_voidSendCommand(u8 Copy_u8Command)
 {
@@ -23,11 +26,11 @@ void CLCD_voidSendCommand(u8 Copy_u8Command)
 
 	DIO_enumSetPortValue(CLCD_DATA_PORT,Copy_u8Command);
 
-	/*Set Rs Pin To low For  to send  command for CLCD*/
+	/*Clear Rs Pin to send  command for CLCD*/
 
 	DIO_enumSetPinValue(CLCD_CTRL_PORT,CLCD_RS_PIN,DIO_PIN_LOW);
 
-	/*Set Rw Pin To low For  to write */
+	/*Clear Rw Pin to write */
 
 	DIO_enumSetPinValue(CLCD_CTRL_PORT,CLCD_RW_PIN,DIO_PIN_LOW);
 
@@ -50,7 +53,7 @@ void CLCD_voidSendData(u8 Copy_u8Data)
 
 	DIO_enumSetPinValue(CLCD_CTRL_PORT,CLCD_RS_PIN,DIO_PIN_HIGH);
 
-	/*Set Rw Pin To low For  to write */
+	/*Clear Rw Pin to write */
 
 	DIO_enumSetPinValue(CLCD_CTRL_PORT,CLCD_RW_PIN,DIO_PIN_LOW);
 
@@ -84,13 +87,109 @@ void CLCD_Init(void)
 
 }
 
-void CLCD_SendString(const u8 *ptString)
+/*
+* Breif : This Function send string to the port which is defined in config.h
+* Parameters :
+            => * Copy_u8ptString  --> Pointer to the string
+* return : nothing
+*/
+
+void CLCD_SendString(const u8 *Copy_u8ptString)
 {
-	u8 * localChar = ptString;
-	while(*localChar!='\0')
+	u8  localChar ;
+	while(Copy_u8ptString[localChar]!='\0')
 	{
-		CLCD_voidSendData(*ptString);
+		CLCD_voidSendData(Copy_u8ptString[localChar]);
 		localChar++;
 	}
 }
+
+/*
+* Breif : This Function set the curser position
+* Parameters :
+            => Copy_u8Row --> row number (CLCD_ROW_1 or CLCD_ROW_2)
+			=> Copy_u8Col --> column number (CLCD_COL_1 ... CLCD_COL_16)
+* return : nothing
+*Hint :-
+   In This function we send a command which =0b1xxxxxxx
+   MSB = 1  ===> refers that it is command to set cursor
+   xxxxxxx  ===> refers to AC ( Address Counter 7Bits / DDRAM Locations 128Location )
+*/
+void CLCD_SendPosition(u8 Copy_u8Row,u8 Copy_u8Col)
+{ 	u8 Loc_u8Address;
+	if(0 == Copy_u8Row)
+	{
+		Loc_u8Address = Copy_u8Col;
+	}
+	else if(1 == Copy_u8Row)
+	{
+		Loc_u8Address=Copy_u8Col+0x40;
+	}
+	CLCD_voidSendCommand(Loc_u8Address);
+
+}
+/* Breif : This Function send extra char
+* Parameters :
+            => Copy_u8Row --> row number (CLCD_ROW_1 or CLCD_ROW_2)
+			=> Copy_u8Col --> column number (CLCD_COL_1 ... CLCD_COL_16)
+* return : nothing
+* Hint :-
+	Address Counter can refer to CGRAM and DDRAM
+*/
+
+void CLCD_WriteSpecialCharacter(u8 Copy_u8Row,u8 Copy_u8Col)
+{
+	u8 LOC_u8Iterator = 0 ;
+	/* 1- Go To CGRAM            */
+	CLCD_voidSendCommand( clcd_CGRAM );  // Make AC refers to the first Place/Address at CGRAM
+
+
+	/* 2- Draw Character in CGRAM        */
+	/* Hint : it will be copied to DDRAM automatically */
+	for( LOC_u8Iterator = 0 ; LOC_u8Iterator < sizeof(CLCD_u8SpecialChar) / sizeof(CLCD_u8SpecialChar[0]) ; LOC_u8Iterator++){
+
+		CLCD_voidSendData( CLCD_u8SpecialChar[LOC_u8Iterator] );
+
+	}
+
+
+	/* 3- Back (AC) To DDRAM          */
+	CLCD_SendPosition(Copy_u8Row,Copy_u8Col);
+
+
+	/* 4- Send Character Address */
+	for( LOC_u8Iterator = 0 ; LOC_u8Iterator < 8 ; LOC_u8Iterator++ ){
+
+		CLCD_voidSendData( LOC_u8Iterator );
+
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
